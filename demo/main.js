7baca157ce5c641499f7ae1826f8d7b1e1f01095
Michael Paszkiewicz <mike@tryhabitat.com>
2016-04-27 15:45:35 -0400

    got turf.inside, background geolocation for current user

--- demo/main.js
+++ demo/main.js
@@ -1,67 +1,63 @@
 FlowRouter.route('/', {
   action: function(params) {
-    FlowLayout.render('Layout', {content: 'Main'});
-  }
-});
-
-FlowRouter.route('/gl', {
-  action: function(params) {
     FlowLayout.render('Layout', {content: 'GL'});
   }
 });
 
 if (Meteor.isClient) {
-  Template.Main.onRendered(function () {
-    Mapbox.debug = true;
-    Mapbox.load({
-      plugins: ['markercluster', 'heat']
-    });
-
-    this.autorun(function () {
-      if (Mapbox.loaded()) {
-        L.mapbox.accessToken = 'pk.eyJ1IjoicGF1bG9ib3JnZXMiLCJhIjoicFQ1Sll5ZyJ9.alPGD574u3NOBi2iiIh--g';
-        var map = L.mapbox.map('map', 'pauloborges.k8eg55fh');
-      }
-    });
-  });
-
-  Template.Main.helpers({
-    html: '<div id="map" class="mapbox"></div>',
-    js:   'Mapbox.load();\nTracker.autorun(function () {\n' +
-          '\tif (Mapbox.loaded()) {\n' +
-          '\t\tL.mapbox.accessToken = MY_ACCESS_TOKEN;\n' +
-          '\t\tvar map = L.mapbox.map("map", MY_MAP_ID);\n' +
-          '\t}\n' +
-          '});'
-  });
+  var source,
+      map;
 
   Template.GL.onRendered(function () {
-    Mapbox.debug = true;
-    Mapbox.load({
-      gl: true
-    });
+    Mapbox.debug = false;
+    Mapbox.load({ gl: true });
 
-    this.autorun(function () {
+    this.autorun(function() {
       if (Mapbox.loaded()) {
-        mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bG9ib3JnZXMiLCJhIjoicFQ1Sll5ZyJ9.alPGD574u3NOBi2iiIh--g';
-        var map = new mapboxgl.Map({
-          container: 'map',
-          style: 'mapbox://styles/mapbox/streets-v8',
+        //test token given on mapbox examples
+        mapboxgl.accessToken = 'pk.eyJ1IjoiaGFiaXRhdG1pa2UiLCJhIjoiY2lsdHVpMWQxMDBjdnVza3NyNnV6Y3N3NCJ9.dWupCOy48vAo8BocejIdtg';
+
+        //set initial source data...still haven't found a meteor workaround
+        //need an inital source for map.addSource onLoad
+        source = new mapboxgl.GeoJSONSource({
+          data: 'https://wanderdrone.appspot.com/'
+        });
+
+        map = new mapboxgl.Map({
+            container: 'map',  //div id of map container
+            style: 'mapbox://styles/mapbox/streets-v8', //pick mapbox style
+            center: temple.center, //geoJSON of center (not just lng, lat)
+            zoom: 12, //zoom
+            pitch: 60, //angle of tilt from directly downward
+            bearing: -3, // offset from 'true north'
+        }).on('load', function () {
+
+          render.schoolBounds(map);
+
+          //render live runner marker, need to pass inital source up AFAIK (this is just proof of concept)
+          render.runnerLocation(map, source);
+
+          //render an inside and outside student
+          //is static fixture data now ->
+            //for the delivery address selector, want to use a tracker on student's coords
+          render.students(map);
         });
       }
     });
   });
 
-  Template.GL.helpers({
-    html: '<div id="map" class="mapbox"></div>',
-    js:   'Mapbox.load({gl: true});\nTracker.autorun(function () {\n' +
-          '  if (Mapbox.loaded()) {\n' +
-          '    mapboxgl.accessToken = MY_ACCESS_TOKEN;\n' +
-          '      var map = new mapboxgl.Map({\n' +
-          '      container: "map",\n' +
-          '      style: "https://www.mapbox.com/mapbox-gl-styles/styles/outdoors-v7.json",\n' +
-          '    };\n' +
-          '  }\n' +
-          '});'
-  });
+  //using window.setInterval per docs
+  //https://www.mapbox.com/mapbox-gl-js/example/live-geojson/
+
+  //With actual data, this would be a Tracker observing changes
+  //on the runner's position. Position would be update in method call
+  //like seen in geoloc.init.js
+  window.setInterval(function() {
+    var lngLat = {
+      lng: Session.get('longitude'),
+      lat: Session.get('latitude')
+    };
+
+    source.setData(transformToGJSON(lngLat));
+  }, 2000);
 }
